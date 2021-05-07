d4c28f51783241dd201ecbeda80ee4e4c958beee
anas12k <47221061+anas12k@users.noreply.github.com>
2021-05-08 01:56:45 +0530

    javascript

---
+++ javascript.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+    getTime();
+}
+function getTime(){
+    var today=new Date();
+    var hours=today.getHours();
+    var minutes=today.getMinutes();
+    var second=today.getSeconds();
+
+    minutes=checkTime(minutes);
+    second=checkTime(second);
+    document.getElementById('txt').innerHTML=hours+":"+minutes+":"+second;
+    getTimeOut(function(){
+        getTime()},10000
+    );
+}
+function checkTime(i){
+    if(i<10){
+        i="0"+i;
+    }
+    return i;
+}
