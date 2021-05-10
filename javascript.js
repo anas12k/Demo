43a13bb8a5b7de1f518c7c8f969b0f996b51620d
anas12k <47221061+anas12k@users.noreply.github.com>
2021-05-11 01:38:54 +0530

    change

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@ let time;
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 setInterval(() => {
-    time=checkTime(time);
     a=new Date();
     date=a.toLocaleDateString(undefined,options);
     time=a.getHours()+ ':' +a.getMinutes()+ ':' +a.getSeconds();
